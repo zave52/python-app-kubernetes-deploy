5d79f0bda4611947dee9900bdcc581a4cc65ca91
Zakhar Savchyn <zakhar.savchyn.dev@gmail.com>
2025-12-22 22:28:15 +0200

    configured Terraform remote backend with GCS for state management

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "gcs" {
+    bucket  = "python-app-kubernetes-backend"
+    prefix  = "dev/python-restful-api"
+  }
+
   required_providers {
     google = {
       source = "hashicorp/google"
